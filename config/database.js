cd17d0364e92d83146da38340fac6f97d750fd32
R0dner <ronisp200@gmail.com>
2025-07-18 21:41:51 -0400

    PRUEBA1

--- config/database.js
+++ config/database.js
@@ -2,13 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db-jacquard'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '69827095roni'),
-      schema: 'public',
-      ssl: false  
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', false)
     },
     debug: false,
   },

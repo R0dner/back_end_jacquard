de5a2a2cecdceb708b3661611cb18b1f5cc70cd5
R0dner <ronisp200@gmail.com>
2025-07-30 14:41:40 -0400

    prueba cambio en database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'https://delicate-attraction-2c7f961647.strapiapp.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'db-jacquard'),
       user: env('DATABASE_USERNAME', 'postgres'),

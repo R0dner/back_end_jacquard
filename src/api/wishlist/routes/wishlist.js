183f2f3e9dfb356ca69d12db6afcdb717c07d9db
R0dner <ronisp200@gmail.com>
2025-10-14 22:11:38 -0400

    errores cambio

--- src/api/wishlist/routes/wishlist.js
+++ src/api/wishlist/routes/wishlist.js
@@ -1,32 +1,7 @@
-'use strict';
+// src/api/wishlist/routes/wishlist.js
 
-/**
- * wishlist router
- */
+'use strict';
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-// Rutas por defecto
-const defaultRouter = createCoreRouter('api::wishlist.wishlist');
-
-// Rutas personalizadas
-const customRoutes = {
-  routes: [
-    {
-      method: 'DELETE',
-      path: '/wishlists/producto/:productoId',
-      handler: 'wishlist.deleteByProduct',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
-
-module.exports = {
-  routes: [
-    ...defaultRouter.routes,
-    ...customRoutes.routes,
-  ],
-};
+module.exports = createCoreRouter('api::wishlist.wishlist');

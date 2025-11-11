8473770e388017c9590dc0020d638e56594062d0
R0dner <ronisp200@gmail.com>
2025-11-10 21:22:33 -0400

    cambio en data base

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,16 @@
 // path: ./config/env/production/server.js
-
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
-  app: { 
-    keys: env.array('APP_KEYS')
+  url: env('APP_URL', 'https://delicate-attraction-2c7f961647.strapiapp.com'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });

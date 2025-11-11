360cebc2b480b3d5fb6bbe47e3f9f52f9f147354
R0dner <ronisp200@gmail.com>
2025-11-10 20:59:10 -0400

    prueba de base de datos

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,14 +3,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-      },
+      host: env('DATABASE_HOST', 'delicate-attraction-2c7f961647.strapiapp.com'), // sin https://
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'db-jacquard'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '69827095roni'),
+      ssl: false, // o agrega configuración si tu hosting lo requiere
     },
     debug: false,
   },
--- src/api/wishlist/routes/wishlist.js
+++
@@ -1,7 +0,0 @@
-// src/api/wishlist/routes/wishlist.js
-
-'use strict';
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::wishlist.wishlist');

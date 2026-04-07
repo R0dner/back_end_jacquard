741983ba090bc99d18a13bbbc36514df788780c3
R0dner <ronisp200@gmail.com>
2026-04-06 20:21:00 -0400

    prueba 2 de base de datos

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,14 @@
 // path: ./config/env/production/server.js
 
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('APP_URL'), 
-  app: { 
-    keys: env.array('APP_KEYS')
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1e978b20a8532605b42d18bf01ea37f4'),
+    },
   },
 });

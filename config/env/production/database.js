43e799f1ea6e27417277ff42ba921df26a5d2c77
R0dner <ronisp200@gmail.com>
2026-04-06 20:12:09 -0400

    prueba de base de datos

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,16 @@
 // path: ./config/env/production/database.js
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-      },
+      host: env('DATABASE_HOST', 'https://delicate-attraction-2c7f961647.strapiapp.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'db-jacquard'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '69827095roni'),
+      schema: 'public',
+      ssl: false  
     },
     debug: false,
   },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@
 
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+  url: env('APP_URL'), 
   app: { 
     keys: env.array('APP_KEYS')
   },

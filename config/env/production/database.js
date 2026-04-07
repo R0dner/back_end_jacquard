439e050f6a5bff55b898c9360ec633823684098b
R0dner <ronisp200@gmail.com>
2026-04-06 20:38:43 -0400

    volviendo al inciio

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,15 @@
 // path: ./config/env/production/database.js
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'delicate-attraction-2c7f961647.strapiapp.com'), // ❌ sin https://
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db-jacquard'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '69827095roni'),
-      schema: 'public',
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
-        rejectUnauthorized: false  
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
     },
     debug: false,
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,9 @@
 // path: ./config/env/production/server.js
 
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1e978b20a8532605b42d18bf01ea37f4'),
-    },
+  proxy: true,
+  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+  app: { 
+    keys: env.array('APP_KEYS')
   },
 });
